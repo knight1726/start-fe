33a6524eca68b11119183a06c8d6208b4e24b135
knight1726 <62638001+knight1726@users.noreply.github.com>
2020-04-08 00:43:46 +0900

    과제

---
+++ 01/app.js
@@ -0,0 +1,61 @@
+
+console.log('app.js');
+
+// var name = '박준범';
+// console.log(name);
+
+// console.log(1);
+// console.log(2);
+// var count = 1;
+// console.log(count);
+
+// var isMan = true;
+//var isMan = "true"//String
+//console.log(isMan,true);
+
+
+// function log(str){
+//     console.log(str);
+// }
+// var log = function(str){
+//     console.log(str);
+// };
+
+
+// var image ={
+//     width : 100,
+//     height : 100,
+//     name :'1.png',
+// } ;
+
+
+// var image1 = [1, 2, 3, 4, 5, 6, 'a', {}];
+// image[0] = 'a';
+// var data = null;
+// var data1 = "";
+// var data2;
+// console.log(data, data1, data2);
+
+// var imageWidth = 100;
+// var imageHeight = 100;
+// var imageName = '1.png';
+//console.log(image, image1);
+
+
+// console.log(1 + 2);
+// console.log('a'+'b');
+//var count = 1;
+//var number = '2';
+
+
+//console.log(count + number);
+
+// var isHero = true;
+// if(isHero){
+//     console.log('hero');
+// } 
+
+// console.log(typeof 1);
+// console.log(typeof {});
+// console.log(typeof '1');
+// console.log(typeof {});
---
+++ 02/app.js
@@ -0,0 +1,72 @@
+console.log('app');
+
+// var todos = ["운동"];
+// var todo = ['게임'];
+// todos.push(todo);
+// console.log(todos);
+// todos.forEach(function(todo){
+//     console,log(todo)
+// });
+
+// var todos = ['운동', '게임'];
+// var updateTodo = '게임';
+// var updateIndex = todos.findIndex(function(todo) {
+//   return todo === updateTodo;
+// });
+// // console.log(tupdateIndex);
+// todos[updateIndex] = '공부';
+// console.log(todos);
+
+// var todos = ['운동', '공부', '목욕'];
+// var deleteTodo = '공부';
+// var deleteIndex = todos.findIndex(function(todo) {
+//   return todo === deleteTodo;
+// });
+// todos.splice(deleteIndex, 1);
+// console.log(todos);
+
+
+// var isMan = true;
+// if(isMan) {
+//     close.log('1');
+// }
+// else if(isMan && isHero){
+
+// }
+// else {
+//     console.log('2');
+// }
+
+// window.alert('안녕하세요');
+// var who = prompt('당신은 누구입니까?');
+// console.log(who);
+
+// var isDelete = confirm('삭제하시겠습니까?');
+
+// console.log(isDelete);
+
+// function sum(a,b) {
+//     return a +b;
+// }
+// console.log{sum(1,2)};
+
+// var car = {
+//     year: '2014',
+//     starting: function() {
+//       console.log('starting..');
+//     },
+//   };
+  
+//   car.starting();
+
+// (function() {
+//     var a = 'a';
+//     console.log(a);
+// })();
+
+// function sum(x, y) {
+//     if (!y) y = 1;
+//     console.log(x + y);
+//   }
+//  console.log(sum(3));
+//  console.log(sum(3,3));
