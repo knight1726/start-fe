ddf5e2d7a500bb3309d990bab2164b4287356b59
knight1726 <62638001+knight1726@users.noreply.github.com>
2020-05-14 13:06:20 +0900

    퀴즈

---
+++ 07/guiz08.js
@@ -0,0 +1,30 @@
+const  $ min  =  document . getElementById ( 'min' ) ;
+const  $ max  =  document . getElementById ( 'max' ) ;
+const  $ btn  =  document . getElementById ( 'btn' ) ;
+const  $ box  =  document . getElementById ( 'box' ) ;
+
+randomnum  =  0으로 ;
+하자  boxnum = 0 ;
+
+getrandomnum ( )  함수 {
+  CONST  분 = 에서는 parseInt ( $ 분 . 값 ,  10 ) ;
+  CONST  최대 = 에서는 parseInt ( $ 맥스 . 값 ,  10 ) ;
+  randomnum  = 수학 . 층 ( 수학 . 랜덤 ( ) * ( 최대  -  최소  +  1 ) )  +  분 ;
+   난수를 반환 ;
+}
+
+$ btn . addEventListener ( 'click' ,  ( )  =>  {
+  getrandomnum ( ) ;
+  boxnum  =  randomnum  -  40 ;
+
+  const  타이머  =  setInterval ( ( )  =>  {
+    if  ( 난수 ! == boxnum )  {
+      박스 넘  + =  1 ;
+      $ btn . 비활성화 됨  =  true ;
+      $ box . innerHTML  =  boxnum ;
+    }  else  {
+      $ btn . 비활성화  =  거짓 ;
+      clearInterval ( 타이머 ) ;
+    }
+  } ,  20 ) ;
+} ) ;
